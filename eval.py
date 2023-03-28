e699270e59016d6e0dbfcf5e75c071b2cd454b61
Yusuke Goto <sample@email.com>
2023-03-29 01:31:47 +0900

    評価器を固定

--- eval.py
+++ eval.py
@@ -1,54 +1,62 @@
 import subprocess
 from tqdm import tqdm
-import time
-def main():
+from concurrent.futures import ThreadPoolExecutor
+
+tot_min = 20
+def calc(d):
     sum_score = 0.0
-    norm = 0
-    max_time = 0.0
-    max_time_problem = None
     worst_score = 0
     worst_score_problem = None
+    norm = 0
+    for i in tqdm(range(1, tot_min * 60 // 5)):
+        cmd = "./scan eval < tools/" + "in{}".format(d) + "/{0:04d}.txt".format(i)
+        #cmd = "./pre_bin eval < tools/" + "in{}".format(d) + "/{0:04d}.txt".format(i)
+        rets = subprocess.getoutput(cmd)
+        try:
+            score = int(rets.split("\n")[-1])
+        except:
+            print("conversion falied.")
+            print("=========================")
+            print(cmd)
+            print("=========================")
+            print(type(rets.split("\n")))
+            print((rets.split("\n")))
+            print(int(rets.split("\n")[-1]))
+            print(rets)
+            print("=========================")
+            return
+        if worst_score < score:
+            worst_score = score
+            worst_score_problem = (d, i)
+        sum_score += score
+        norm += 1
+    ave_score = sum_score / norm
+    return ave_score, worst_score, worst_score_problem
+def main():
     ini_cmd = "cargo build --release"
     print(subprocess.getoutput(ini_cmd))
-    for d in range(5, 14 + 1):
-        print("executing: d = ", d)
-        sum_score0 = 0.0
-        norm0 = 0
-        max_time0 = 0
-        tot_min = 60 * 3
-        for i in tqdm(range(1, tot_min * 60 // 10 // 5)):
-            cmd = "./target/release/ahc019 eval < tools/" + "in{}".format(d) + "/{0:04d}.txt".format(i)
-            #cmd = "./pre_bin eval < tools/" + "in{}".format(d) + "/{0:04d}.txt".format(i)
-            time0 = time.time()
-            rets = subprocess.getoutput(cmd)
-            time1 = time.time()
-            try:
-                score = int(rets.split("\n")[-1])
-                max_time0 = max(max_time0, time1 - time0)
-            except:
-                print("conversion falied.")
-                print("=========================")
-                print(cmd)
-                print("=========================")
-                print(type(rets.split("\n")))
-                print((rets.split("\n")))
-                print(rets)
-                print("=========================")
-                return
-            if worst_score < score:
-                worst_score = score
-                worst_score_problem = (d, i)
-            if max_time < max_time0:
-                max_time = max_time0
-                max_time_problem = (d, i)
-            sum_score0 += score
-            norm0 += 1
-        ave_score0 = sum_score0 / norm0
-        print(ave_score0, worst_score_problem, max_time, max_time_problem)
-        sum_score += sum_score0
-        norm += norm0
+    cp_cmd = "cp target/release/ahc019 scan"
+    print(subprocess.getoutput(cp_cmd))
+    THREAD = 10
+    futures = []
+    with ThreadPoolExecutor(max_workers = THREAD, thread_name_prefix = "thread") as pool:
+        for d in range(5, 14 + 1):
+            future = pool.submit(calc, d)
+            futures.append(future)
+    norm = 0
+    sum_score = 0
+    worst_score = 0
+    worst_score_problem = None
+    for (i, future) in enumerate(futures):
+        d = i + 5
+        ave_score0, worst_score0, worst_score_problem0 = future.result()
+        print("d={}".format(d), "ave:", ave_score0, "worst:", worst_score0, "worst_at:", worst_score_problem0)
+        if worst_score < worst_score0:
+            worst_score_problem = worst_score_problem0
+        sum_score += ave_score0
+        norm += 1
     ave_score = sum_score / norm
-    print(ave_score * 50, worst_score_problem, max_time, max_time_problem)
+    print(ave_score * 50, worst_score, worst_score_problem)
 
 if __name__ == "__main__":
     main()
